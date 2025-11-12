487f9e7685d2740e9251055cd2625621ea5091b1
Abdulmalik Abdullah <abdullahtemitope1@gmail.com>
2025-11-13 00:20:15 +0100

    Core features of Airbnb

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,10 @@
+    const Button = ({ title, styles }) => {
+        return (
+            <button  className={`px-4 py-2 font-semibold text-white bg-blue-600 hover:bg-blue-700 transition ${styles}`}
+    >
+        {title}
+            </button>
+        )
+    }
+
+    export default Button;
---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,30 @@
+import Card from "@/components/Card";
+import Button from "@/components/Button";
+
+const Landing: React.FC =  () => {
+  return (
+    <div>
+      <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <Card />
+      <div className="flex flex-col gap-4 items-center justify-center min-h-screen bg-gray-50">
+        <h1 className="text-2xl font-bold mb-4">Landing Buttons</h1>
+
+        {/* Small Butttons */}
+        <button title="small rounded-sm"></button>
+        <button title="small rounded-md"></button>
+        <button title="small rounded-full"></button>
+
+        {/* Medium Buttons */}
+        <button title="medium rounded-sm"></button>
+        <button title="medium rounded-md"></button>
+        <button title="medium rounded-full"></button>
+
+        {/* Large Buttons */}
+        <button title="large rounded-sm"></button>
+        <button title="large rounded-md"></button>
+        <button title="large rounded-full"></button>
+      </div>
+    </div>
+  )
+}
+export default Landing;
